c5e0d0485b5dec6c3f34e6c05e8ad148e0e693c3
ryoneue <ryoneue@gmail.com>
2022-11-04 15:27:34 +0900

    Make LINE class and LINE.py

--- main.py
+++ main.py
@@ -1,115 +1,15 @@
-from encodings.utf_8 import encode
-import numpy as np
-import pandas as pd
-import glob
-import copy
-import MeCab
-import unidic
-import tqdm
-import os
-import re
-import datetime
-
-def cleanLine(line):
-    tmp = copy.copy(line)
-    tmp = tmp.replace("\n", "")
-    return tmp
-
-def parseLine(line, num):
-    parse = {}
-    parse["num"] = num
-    tagger = MeCab.Tagger()
-    tags = re.findall('\[.{1,5}\]', line)
-    parse["tag"] = tags
-    
-    for tag in tags:
-#        print(line, tag, tags)
-        line = line.replace(tag, " replace")
-#        print(line)
-    if "保存日時:" in line:
-        date = datalist[1].split("保存日時")[-1][1:-1]
-        date_f = datetime.datetime.strptime(date, '%Y/%m/%d %H:%M')
-        parse["date"] = date_f
-
-    elif "[LINE]" in line:
-        print("LINE")
-        
-    elif  len(line.split("\t")) == 3:        
-        time, name, sentence = line.split("\t")
-        parse["time"] = time
-        parse["name"] = name
-        parse["sentence"] = sentence
-        parse["Msentence"] = tagger.parse(sentence) 
-        
-    return parse
-
-def wordCount(cleanData):
-    countDict = {}
-    for parse in cleanData:
-        if "Msentence" in parse.keys():
-            Msentence = parse["Msentence"]
-            Wakati = Msentence.split("\n")
-            for wakati in Wakati:
-#                print(wakati)
-                if wakati == "EOS":
-                    break
-                word = wakati.split("\t")[0]
-                if word in countDict.keys():
-                    countDict[word] += 1
-                else:
-                    countDict[word] = 1
-    return countDict
-    
-def wordSelect(cleanData, morp):
-    countDict = {}
-    for parse in cleanData:
-        if "Msentence" in parse.keys():
-            Msentence = parse["Msentence"]
-#            print(Msentence)
-#            tags = re.finditer('\[.*\]', Msentence)
-#            for tag in tags:
-#                Msentence.replace(tag, " ")
-                
-            
-            Wakati = Msentence.split("\n")
-            for wakati in Wakati:
-#                print(wakati)
-#                if re.search('\[.*\]', wakati):
-#                    print(wakati)
-#                    continue
-                    
-                if wakati == "EOS":
-                    break                
-                word = wakati.split("\t")[0]
-                wakati_morp = wakati.split("\t")[1].split(",")[0]
-                
-
-                if wakati_morp == morp:
-                    if word in countDict.keys():
-                        countDict[word] += 1
-                    else:
-                        countDict[word] = 1
-    return countDict
-
+from LINE import LINE
            
 
 txt = "data/line_utf-8.txt"
 # data = np.loadtxt(txt, encoding="utf-8",dtype="str" ,delimiter='\n')
 datapath = "cleanData.npy"
-if not os.path.exists(datapath):
-    with open(txt,encoding=("utf-8")) as f:
-        datalist = f.readlines()
-        
-    cleanData = []
-    for num, line in tqdm.tqdm(enumerate(datalist), total=len(datalist)):
-        line = cleanLine(line)
-    #    print(line)
-        parse = parseLine(line, num)
-        cleanData.append(parse)
-    np.save("cleanData.npy", cleanData)
-        
-else:
-    cleanData = np.load(datapath)
-countDict = wordCount(cleanData)
-countDict_meishi = wordSelect(cleanData, "名詞")
+
+LineData = LINE(datapath)
+
+
+    
+
+countDict = LineData.wordCount()
+countDict_meishi = LineData.wordSelect("名詞")
 sorted_dict = sorted(countDict_meishi.items(), key = lambda item: item[1])
